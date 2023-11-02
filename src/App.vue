8beecbd4d5ce793e1fd92f90952b41376fc25390
Andrea López Suárez <60515012+andrealps@users.noreply.github.com>
2023-11-02 10:37:18 +0000

    Remove unneccesary code

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,27 @@
 <script setup lang="ts">
-  import HelloWorld from './components/HelloWorld.vue';
-  import TheWelcome from './components/TheWelcome.vue';
+  import { ref } from 'vue';
+  import RoundButton from './components/RoundButton.vue';
+
+  const baseClass = 'app';
+
+  const count = ref(0);
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <header>My app</header>
   <main>
-    <TheWelcome />
+    <h1 :class="`${baseClass}__title`">Welcome, User!</h1>
+    <div :class="`${baseClass}__content`">
+      <p :class="`${baseClass}__content-count`">The count is: {{ count }}</p>
+      <div :class="`${baseClass}__content-buttons`">
+        <round-button id="add" icon="+" />
+        <round-button id="add" icon="-" />
+      </div>
+    </div>
   </main>
 </template>
 
-<style scoped>
-  header {
-    line-height: 1.5;
-  }
-
-  .logo {
-    display: block;
-    margin: 0 auto 2rem;
-  }
-
-  @media (min-width: 1024px) {
-    header {
-      display: flex;
-      place-items: center;
-      padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-      margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-      display: flex;
-      place-items: flex-start;
-      flex-wrap: wrap;
-    }
+<style scoped lang="scss">
+  .app {
   }
 </style>
---
+++ src/components/RoundButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button :class="baseClass" @click="emit('click', props.id)">{{ props.icon }}</button>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps<{
+    id: string;
+    icon: string;
+    tooltip?: string;
+  }>();
+  const emit = defineEmits<{
+    click: [value: string];
+  }>();
+
+  const baseClass = 'round-button';
+</script>
+
+<style scoped lang="scss">
+  .round-button {
+    border-radius: 50%;
+  }
+</style>
